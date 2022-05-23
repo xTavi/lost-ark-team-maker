e4766a48c018c16d1e4cd50e342a37c03b6cd929
Octavian Constantin <octavian.constantin@bearingpoint.com>
2022-05-23 20:12:16 +0300

    Added snack bar, validation, adding team mebers, maximum team limit of 8, added grid and special colors for supports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
-import { TeamComponent } from './team/team.component';
+import {
+  TeamComponent,
+  TeamMemberCreateDialogComponent,
+} from './team/team.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { TeamMemberComponent } from './team/team-member/team-member.component';
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   imports: [
@@ -26,8 +33,19 @@ import { TeamMemberComponent } from './team/team-member/team-member.component';
     MatButtonModule,
     MatIconModule,
     MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
   ],
-  declarations: [AppComponent, DragDropComponent, TeamComponent, TeamMemberComponent],
+  declarations: [
+    AppComponent,
+    DragDropComponent,
+    TeamComponent,
+    TeamMemberComponent,
+    TeamMemberCreateDialogComponent,
+  ],
   bootstrap: [AppComponent],
   providers: [],
 })
